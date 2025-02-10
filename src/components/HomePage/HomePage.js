634f0d33e2ac2f1848b51b28bdd61e128c40ed5e
Sherry <sm718036@gmail.com>
2025-02-10 16:26:47 +0500

    last commit

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap
+import "/public/static/home.css";
+
+const HomePage = () => {
+  return (
+    <div className="container my-5 text-center">
+      <h1 className="display-4 mb-3">GiftLink</h1>
+      <h2 className="mb-4">Share Gifts and Joy!</h2>
+      <p className="lead">
+        "Sharing is the essence of community. It is through giving that we
+        enrich and perpetuate both our lives and the lives of others."
+      </p>
+      <a href="/app" className="btn btn-primary">
+        Get Started
+      </a>
+    </div>
+  );
+};
+export default HomePage;
