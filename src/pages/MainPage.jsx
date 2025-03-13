ecba74a796c3a94b3611e39cb736455760464a16
Sherry <sm718036@gmail.com>
2025-03-13 11:08:47 +0500

    implement laoding and error functionality while loading products

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { urlConfig } from "../config";
 
@@ -14,7 +14,6 @@ function MainPage() {
       const res = await fetch(`${urlConfig.backendUrl}/api/gifts`);
       const data = await res.json();
       setGifts(data);
-      console.log(data)
       setLoading(false);
     } catch (error) {
       setGifts([]);
@@ -47,20 +46,37 @@ function MainPage() {
   return (
     <div className="pt-20 min-h-screen flex flex-col justify-center items-center text-center relative overflow-hidden">
       <div className="absolute inset-0 bg-gradient-to-r from-purple-700 to-blue-500 blur-lg -z-10"></div>
-      <div className="relative p-10 shadow-xl rounded-xl w-full max-w-6xl">
-        <h1 className="text-4xl font-bold text-yellow-400 drop-shadow-md">
-          Available Gifts
-        </h1>
-        <p className="text-lg italic text-gray-200 mt-2">
-          "Find what you need, share what you don't."
-        </p>
-        {loading ? (
-          <p className="ml-4 text-lg font-semibold text-white">
-            Loading Products...
+      {loading ? (
+        <div class="w-full flex items-center justify-center h-fit text-white">
+          <div>
+            <h1 class="text-2xl flex items-center">
+              L
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 24 24"
+                class="animate-spin"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM13.6695 15.9999H10.3295L8.95053 17.8969L9.5044 19.6031C10.2897 19.8607 11.1286 20 12 20C12.8714 20 13.7103 19.8607 14.4956 19.6031L15.0485 17.8969L13.6695 15.9999ZM5.29354 10.8719L4.00222 11.8095L4 12C4 13.7297 4.54894 15.3312 5.4821 16.6397L7.39254 16.6399L8.71453 14.8199L7.68654 11.6499L5.29354 10.8719ZM18.7055 10.8719L16.3125 11.6499L15.2845 14.8199L16.6065 16.6399L18.5179 16.6397C19.4511 15.3312 20 13.7297 20 12L19.997 11.81L18.7055 10.8719ZM12 9.536L9.656 11.238L10.552 14H13.447L14.343 11.238L12 9.536ZM14.2914 4.33299L12.9995 5.27293V7.78993L15.6935 9.74693L17.9325 9.01993L18.4867 7.3168C17.467 5.90685 15.9988 4.84254 14.2914 4.33299ZM9.70757 4.33329C8.00021 4.84307 6.53216 5.90762 5.51261 7.31778L6.06653 9.01993L8.30554 9.74693L10.9995 7.78993V5.27293L9.70757 4.33329Z"></path>
+              </svg>{" "}
+              ading . . .
+            </h1>
+          </div>
+        </div>
+      ) : errMsg ? (
+        <p className="ml-4 text-lg font-semibold text-white">{errMsg}</p>
+      ) : (
+        <div className="relative p-10 shadow-xl rounded-xl w-full max-w-6xl">
+          <h1 className="text-4xl font-bold text-yellow-400 drop-shadow-md">
+            Available Gifts
+          </h1>
+          <p className="text-lg italic text-gray-200 mt-2">
+            "Find what you need, share what you don't."
           </p>
-        ) : errMsg ? (
-          <p className="ml-4 text-lg font-semibold text-white">{errMsg}</p>
-        ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6 w-full px-4">
             {gifts.map((gift) => (
               <div
@@ -97,8 +113,8 @@ function MainPage() {
               </div>
             ))}
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
